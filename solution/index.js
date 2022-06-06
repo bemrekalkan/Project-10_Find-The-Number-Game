ce3b09d304b1637804e3bac7ee8cc0251af31a0c
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-06 23:34:40 +0300

    random number is created with floor() method.

---
+++ solution/index.js
@@ -0,0 +1,10 @@
+let continueGame;
+let guessNumber;
+
+do{
+    let live=5;
+
+    const randomNumber = Math.floor(Math.random() * 101);
+    //! Returns a random INTEGER (floor()) from 0 to 100:
+    console.log(randomNumber);
+}
