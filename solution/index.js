af15a040e4043ee83007b61a3126f40e5897fa87
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-07 19:13:30 +0300

    app.js is updated.

--- solution/index.js
+++ solution/index.js
@@ -1,34 +1,36 @@
 let continueGame;
 let guessNumber;
 
-do{
-    let live=5;
+do {
+  let live = 5;
 
-    const randomNumber = Math.floor(Math.random() * 101);
-    //! Returns a random INTEGER (floor()) from 0 to 100:
-    console.log(randomNumber);
+  const randomNumber = Math.floor(Math.random() * 101);
+  //! Returns a random INTEGER (floor()) from 0 to 100:
+  console.log(randomNumber);
 
-    do{
-        const guessNumber = Number(prompt("Enter a number between 0 and 100..."));
-        live -= 1;
+  do {
+    const guessNumber = Number(prompt("Enter a number between 0 and 100..."));
+    live -= 1;
 
-        if(guessNumber === randomNumber){
-            alert(`Congrats! Your ${5 - live}th try.`);
-            break;
-        } else {
-            alert(`Be careful! You have only ${live} lives!`);
+    if (guessNumber === randomNumber) {
+      alert(`Congrats! Your ${5 - live}th try.`);
+      break;
+    } else {
+      alert(`Be careful! You have only ${live} lives!`);
 
-            if (guessNumber > randomNumber){
-                alert("DOWN ⬇");
-            } else if (guessNumber < randomNumber){
-                alert(`UP ⬆`);
-            }
-        }
-    } while (live > 0);
-
-    if(live === 0 && guessNumber !== randomNumber){
-        alert(`Sorry, game over..`);
+      if (guessNumber > randomNumber) {
+        alert("DOWN ⬇");
+      } else if (guessNumber < randomNumber) {
+        alert(`UP ⬆`);
+      }
     }
+  } while (live > 0);
 
-    continueGame
-}
+  if (live === 0 && guessNumber !== randomNumber) {
+    alert(`Sorry, game over..`);
+  }
+
+  continueGame = prompt(`Do you wanna play again? (Y/N)`);
+} while (continueGame == "Y" || continueGame == "y");
+
+alert(`See you next time 👋`);
